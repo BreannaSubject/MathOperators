58d3dc03229a4b057e86ec2412109a804a1c4e0c
Breanna Subject <breasubj283@ed.amdsb.ca>
2020-02-11 12:31:07 -0500

    Add project files.

---
+++ MathOperators/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace MathOperators
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.SystemColors.WindowText;
+            this.outputLabel.Font = new System.Drawing.Font("Vineta BT", 20F, System.Drawing.FontStyle.Italic);
+            this.outputLabel.ForeColor = System.Drawing.Color.Chartreuse;
+            this.outputLabel.Location = new System.Drawing.Point(67, 61);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(334, 183);
+            this.outputLabel.TabIndex = 0;
+            this.outputLabel.Text = "HI";
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.startButton.Location = new System.Drawing.Point(12, 12);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Enter";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(479, 295);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.outputLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button startButton;
+    }
+}
+
---
+++ MathOperators/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MathOperators
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            
+            outputLabel.ForeColor = Color.DarkTurquoise;
+            outputLabel.Text = "TV\n";
+            double numberOne, numberTwo, Answer;
+
+            numberOne = 8.8;
+            numberTwo = 2.2;
+
+            Answer = numberOne + numberTwo;
+            
+            outputLabel.Text += Answer + "";
+
+            Answer++;
+
+            outputLabel.Text += "\n" + Answer;
+
+            int numberThree = 13;
+            int modValue = numberThree % 3;
+
+            outputLabel.Text += "\n" + modValue; 
+        }
+
+
+    }
+}
